07b4172c7341c698c87f243c17e223c421d005ca
mansooranis <mansooranis60@gmail.com>
2023-01-08 01:03:33 -0700

    model readme

--- Backend/src/routes/Room.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const Room = require('../controllers/Room')
-
-router.get('/create', Room.createRoom)
-router.post('/delete/:roomCode', Room.deleteRoom)
-
-module.exports = router
